f83104c73da9084d7ba83e498e5be2deb292b381
Giuseppe Luciano Scrimali <s329836@studenti.polito.it>
2026-08-03 21:03:40 +0200

    Add files via upload

---
+++ prony2_en.m
@@ -0,0 +1,120 @@
+% Complex Exponential method including STABILIZATION DIAGRAMS
+% The impulse response is loaded and then
+% the number of modes of interest is selected 
+
+clear all,close all
+
+% measure
+[File, Path]=uigetfile('*.dat','Data file');
+
+if File~=0
+
+  %loading
+  nome=[Path File];
+  load(nome);
+  
+  %extension
+  nome=abs(File);
+  ln=length(nome);
+  for i=1:ln
+    if nome(i)==46
+      np=i-1;
+    end
+  end
+  nome=setstr(nome(1:np));
+  y=eval(nome);
+
+  s=size(y);
+  if s(2)>s(1)
+    y=y';
+  end
+  l=length(y);
+
+  % Sampling period
+  deltat=input(' Sampling period [s]: ');
+  fc=1/deltat;      % Sampling frequency[Hz]
+
+  % time
+  t=[0:deltat:deltat*(l-1)];
+
+  % Impulse response
+%  plot(t,y,'r')
+  %grid on
+
+  % Continua=1   => change the number of modes
+  % Continua=0   => stop
+  figure
+  
+  continua=1;
+
+  while continua==1
+
+    % Number of modes
+    str4=[' MAXIMUM number of modes [<=' int2str(l/4) ']: '];
+    for n=2:input(str4)
+   % n=input(str4);
+
+   % dimension of HH
+    m=2*n;
+
+    % Matrix containing the response
+    H=zeros(2*m,2*n+1);
+    for i=1:2*m
+      H(i,:)=y(i:2*n+i)';
+    end
+
+    % Solution of the system(Ewins: 4.46a)
+    HH=H(:,(1:2*n));
+    Beta=-pinv(HH)*H(:,2*n+1);
+    Beta(2*n+1)=1;
+
+    % Inversion of the order (this is needed by ROOTS)
+    Beta1=flipud(Beta);
+
+    % Compute the roots of the polynomial, whose coefficients are Beta1
+    % (Ewins: 4.40)
+    UR=roots(Beta1)';
+
+    % Roots (Ewins: 4.35)
+    SR=log(UR);
+    omega=abs(SR)/deltat;
+    zeta=-real(SR)./abs(SR);
+
+    % Modal amplitudes (Ewins: 4.47)
+    U=zeros(length(UR),length(UR));
+    for j=1:length(UR)
+      U(j,:)=UR.^(j-1);
+    end
+    AA=inv(U)*y(1:length(UR));
+
+    i=1;
+    k=1;
+    while k<=length(omega)
+  %    if imag(SR(k))<0 & zeta(k)>=0
+        caratt(i,1)=100*zeta(k);
+        caratt(i,2)=omega(k)/(2*pi);
+        caratt(i,3)=1*abs(AA(k));
+        caratt(i,4)=180/pi*angle(AA(k));
+        i=i+1;
+ %     end
+      k=k+1;
+    end
+   % plot(n,caratt(:,2),'*'),hold on
+    plot(caratt(:,2),n,'*'),hold on
+    
+    %disp('smorz.[%] - freq.[Hz] - cost.modale - fase[°]')
+    %disp(caratt)
+ end
+% xlabel('number of modes'),ylabel('freq. [Hz]'),title('frequency stabilization diagram')
+xlabel('freq. [Hz]'),ylabel('number of modes'),title('frequency stabilization diagram')
+ Continue
+    continua=menu('Continue?','Yes','NO');
+    if continua==1, clf,clear caratt,end
+
+  end
+
+else
+  disp(' ')
+  disp(' where is the file?')
+  disp(' ')
+end
---
+++ readme.txt
@@ -0,0 +1,31 @@
+The name of the directory MUST NOT contain any blank spaces!
+
+Matlab codes MUST be executed in the following order:
+
+
+1) Prony_en.m (Complex Exponential method)
+
+
+2) Prony2_en.m (also including stabilization diagrams)
+
+
+3) preliminary analysis of the measurements on the laboratory plate 
+ (please refer to the description file UseOfComplexExpMethod.doc)
+
+write an m-code to:
+  
+  3.1) plot all signals
+  3.2) remove the mean value
+  3.3) save each channel separately with extension .dat 
+  3.4) compute and plot some PSDs to select the best measurements and to recognize the eigenfrequencies 
+
+THEN
+
+  3.5) Prony's method will be  applied to the modified time histories 
+
+
+
+
+
+
+
